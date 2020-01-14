b18a19773ae914144fcfbde9cf5681252941727a
Ashish Pokhrel <apokhre2@byui.edu>
2020-01-14 13:39:33 -0700

    Add project files.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            string myLocation;
+
+            Console.Write("Please enter your name: ");
+            myName = Console.ReadLine();
+
+            Console.Write("Please enter your loation: ");
+            myLocation =  Console.ReadLine();
+
+            Console.WriteLine($"My name is:{myName}");
+            Console.WriteLine($"My location is:{myLocation}");
+
+            var currentDate = DateTime.Now;
+            Console.WriteLine("The current date is: " + currentDate.ToLongDateString());
+
+            var christmas = new DateTime(currentDate.Year, 12, 25);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
